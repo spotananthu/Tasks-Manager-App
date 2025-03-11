8751215cc58f4e60adb8ad46ea8b139ff42a8cbc
Anantha Krishnan <ananthutemp@gmail.com>
2025-03-11 10:36:05 +0530

    first commit

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'mycosmosdb-3ddc6e51';
+const key = 'oqwGY6HqkelHAuZWfGHVDpgnUoX9rkNM7oKLzUAoRZpgqqHO4g8mmvxfqLKpHci9c310Nk3JJbugACDbhfjdCg==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
